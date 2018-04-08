57b17d1ab07a51e57f7ee91451d136cd25a40043
Nilton OS <jniltinho@gmail.com>
2018-04-08 15:50:23 -0300

    add view list users

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import commands
+from functools import wraps
+from flask import Flask, redirect
+from flask import url_for, session, flash
+
+
+# Define the WSGI application object
+app = Flask(__name__)
 
 
 def cmd(shell_cmd):
@@ -12,6 +19,18 @@ def cmd(shell_cmd):
     status, output = commands.getstatusoutput(shell_cmd)
     if status == 0:
         return output.splitlines()
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('home'))
+
+    return wrap
 
 
 def user_create(username, password, given_name, surname):
@@ -26,12 +45,21 @@ def group_list():
     print res
 
 
-def user_list():
-    res = cmd("samba-tool user list")
-    res.remove('krbtgt')
-    print res
+def get_users():
+    users = {'result': []}
+    res = cmd("samba-tool user list|egrep -v 'Guest|krbtgt'")
+    cli = "|".join(res)
+    get_users = cmd("pdbedit -Lf|egrep '%s'" % cli)
+    # print get_users
+    for user in get_users:
+        if user.split(':')[2]:
+           users['result'].append({'username': user.split(':')[0], 'full_name': user.split(':')[2]})
+        else:
+            users['result'].append({'username': user.split(':')[0], 'full_name': user.split(':')[0].title()})
+
+    return users
 
 
 # user_create("nicolas", "Nicolas@2018", "Nicolas", "de Oliveira Silva")
 # group_list()
-user_list()
+# user_list()
--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from flask import url_for, session, abort
 from flask import flash, redirect, render_template, request
 
 from app import app
+from app.utils import login_required,  get_users
 from app.AuthSMB4 import AuthSMB4
 
 
@@ -21,12 +22,20 @@ def welcome():
     return render_template('welcome.html')  # render a template
 
 
+@app.route('/users')
+@login_required
+def users_list():
+    users = get_users()
+    return render_template('users.html', users=users)
+
+
 @app.route('/login', methods=['POST'])
 def login():
     # app.logger.debug("Request Form %s", request.form)
     base = AuthSMB4(request.form['username'], request.form['password'])
     if base.Autenticate():
         session['logged_in'] = True
+        session['username'] = request.form['username']
     else:
         flash('wrong password!')
     return redirect(url_for('home'))
@@ -34,5 +43,6 @@ def login():
 
 @app.route("/logout")
 def logout():
-    session['logged_in'] = False
+    session.clear()
+    # session['logged_in'] = False
     return redirect(url_for('home'))
