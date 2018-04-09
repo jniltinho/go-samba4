4847f805387a4f61a894af6a5a8e34a63151692a
Nilton OS <jniltinho@gmail.com>
2018-04-09 10:17:34 -0300

    add config dev and prod

--- go_samba4.py
+++ go_samba4.py
@@ -17,6 +17,7 @@ openssl req -nodes -new -x509 -keyout ssl/server.key -out ssl/server.crt
 
 import os
 import sys
+import optparse
 import traceback
 from gevent import reinit
 from gevent.wsgi import WSGIServer
@@ -29,10 +30,9 @@ host = "0.0.0.0"
 port = 8088
 
 
-def main():
+def server_prod():
     from app import app
     app.secret_key = os.urandom(12)
-    # app.run(host='0.0.0.0', port=8080)
     try:
         print('Starting Gevent HTTP server on https://%s:%s' % (host, port))
         WSGIServer((host, port), app, keyfile='ssl/server.key',
@@ -42,6 +42,33 @@ def main():
     except Exception:
         traceback.print_exc(file=sys.stdout)
     sys.exit(0)
+
+
+def server_dev():
+    from app import app
+    app.secret_key = os.urandom(12)
+    try:
+        app.run(host=host, port=port, debug=True, ssl_context=('ssl/server.crt', 'ssl/server.key'))
+    except KeyboardInterrupt:
+        print "Shutdown requested...exiting"
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+    sys.exit(0)
+
+
+def main():
+    usage = "Usage: %prog --server-prod|--server-dev"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("--server-prod", action="store_true", dest="SRV_PROD", default=False, help="Server Gevent Prod")
+    parser.add_option("--server-dev", action="store_true", dest="SRV_DEV", default=False, help="Server Flask Desenv")
+
+    options, args = parser.parse_args()
+
+    if (options.SRV_PROD):
+        server_prod()
+
+    if (options.SRV_DEV):
+        server_dev()
 
 
 if __name__ == "__main__":
